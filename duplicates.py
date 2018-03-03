17de184eaf28f5e29bfd7a076ffeb056cd102dd5
Daniil Shadrin <rabbit72rus@gmail.com>
2018-03-03 13:17:32 +0300

    style make simple code

--- duplicates.py
+++ duplicates.py
@@ -1,34 +1,31 @@
 import sys
 import os
+import collections
 
 
-def get_size(dir_path, files_names):
+def get_id_files(dir_path, files_names):
     files_sizes = []
     for file_name in files_names:
         file_path = os.path.join(dir_path, file_name)
         files_sizes.append(os.path.getsize(file_path))
-    return files_sizes
+    id_files = zip(files_names, files_sizes)
+    return id_files
 
 
-def get_all_files(directory):
-    all_files = {}
+def get_all_id_files(directory):
+    all_id_files = collections.defaultdict(list)
     for dir_path, dir_names, files_names in os.walk(directory):
-        if not files_names:
-            continue
-        files_sizes = get_size(dir_path, files_names)
-        id_files = zip(files_names, files_sizes)
+        id_files = get_id_files(dir_path, files_names)
         for id_file in id_files:
-            if id_file not in all_files:
-                all_files[id_file] = []
-            all_files[id_file].append(os.path.abspath(dir_path))
-    return all_files
+            all_id_files[id_file].append(os.path.abspath(dir_path))
+    return all_id_files
 
 
 def get_duplicate_files(directory):
     duplicates = {}
     one_unique_file = 1
-    all_files = get_all_files(directory)
-    for id_file, paths in all_files.items():
+    all_id_files = get_all_id_files(directory)
+    for id_file, paths in all_id_files.items():
         if len(paths) > one_unique_file:
             duplicates[id_file] = paths
     return duplicates
@@ -36,9 +33,7 @@ def get_duplicate_files(directory):
 
 def print_duplicates(duplicate):
     print('Duplicates:')
-    for id_file, paths in duplicate.items():
-        file_name = id_file[0]
-        file_size = id_file[1]
+    for (file_name, file_size), paths in duplicate.items():
         delimiter = '-' * 50
         print(delimiter)
         print(
@@ -52,7 +47,6 @@ def print_duplicates(duplicate):
 
 if __name__ == '__main__':
     search_directory = sys.argv[1]
-    # search_directory = '/home/dany/devman/11_duplicates'
     if not os.path.isdir(search_directory):
         sys.exit('Directory path not available')
     duplicate_files = get_duplicate_files(search_directory)
