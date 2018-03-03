e955d88378ffb5394b2e0a969d16ecf7891b9b42
Daniil Shadrin <rabbit72rus@gmail.com>
2018-03-03 23:46:12 +0300

    update style code

--- duplicates.py
+++ duplicates.py
@@ -8,43 +8,49 @@ def get_size(file_path):
 
 
 def get_files_locations(directory):
-    all_files_locations = collections.defaultdict(list)
+    files_locations = collections.defaultdict(list)
     for dir_path, dir_names, files_names in os.walk(directory):
         for file_name in files_names:
             file_path = os.path.join(dir_path, file_name)
             file_size = get_size(file_path)
-            all_files_locations[(file_name, file_size)].append(file_path)
-    return all_files_locations
+            files_locations[(file_name, file_size)].append(file_path)
+    return files_locations
 
 
-def get_duplicate_files(directory):
+def get_duplicate_files(files_locations):
     duplicates = {}
     one_unique_file = 1
-    files_locations = get_files_locations(directory)
     for (file_name, file_size), paths in files_locations.items():
         if len(paths) > one_unique_file:
             duplicates[(file_name, file_size)] = paths
     return duplicates
 
 
-def print_duplicates(duplicate):
-    print('Duplicates:')
-    for (file_name, file_size), paths in duplicate.items():
-        delimiter = '-' * 50
+def print_duplicates(duplicates):
+    delimiter = '-' * 50
+    if not duplicates:
         print(delimiter)
-        print(
-            'File: {0}\n'
-            'Size: {1}\n'
-            'Paths:'.format(file_name, file_size))
-        print('\n'.join(paths))
+        print('No files duplicates')
         print(delimiter)
+    else:
+        print('Duplicates:')
+        for (file_name, file_size), paths in duplicates.items():
+            print(delimiter)
+            print(
+                'File: {0}\n'
+                'Size: {1}\n'
+                'Paths:'.format(file_name, file_size))
+            print('\n'.join(paths))
+            print(delimiter)
 
 
 if __name__ == '__main__':
-    search_directory = sys.argv[1]
-    if not os.path.isdir(search_directory):
-        sys.exit('Directory path not available')
-    duplicate_files = get_duplicate_files(search_directory)
-    if not duplicate_files:
-        sys.exit('No files duplicates')
-    print_duplicates(duplicate_files)
+    try:
+        search_directory = sys.argv[1]
+        if not os.path.isdir(search_directory):
+            exit('Directory path not available')
+        files_locations = get_files_locations(search_directory)
+        duplicate_files = get_duplicate_files(files_locations)
+        print_duplicates(duplicate_files)
+    except IndexError:
+        exit('No directory path. Try again entering the path')
